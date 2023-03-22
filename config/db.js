36a692baf309a28646102449c298c5ceed494138
deveRana <87362652+deveRana@users.noreply.github.com>
2023-03-22 19:36:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://mohit_the_coder:mohit_the_coder@ac-2ffmw0w-shard-00-00.pstta4e.mongodb.net:27017,ac-2ffmw0w-shard-00-01.pstta4e.mongodb.net:27017,ac-2ffmw0w-shard-00-02.pstta4e.mongodb.net:27017/?ssl=true&replicaSet=atlas-2inpq2-shard-0&authSource=admin&retryWrites=true&w=majority");
     console.log(`MongoDB connected`);
   } catch (error) {
     console.log(error);
