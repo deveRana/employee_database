deb7c7a6a80dacdc35e0d9b7acbcca257c877356
deveRana <87362652+deveRana@users.noreply.github.com>
2023-03-22 19:46:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 const db = require('./config/db');
 const { errorHandler } = require('./middlewares/errorHandler');
 const port = 3000;
-//database connection initiated
-db();
-// Middleware to parse JSON data
-app.use(express.json());
-// Middleware to parse URL-encoded data
-app.use(express.urlencoded({ extended: true }));
+// //database connection initiated
+// db();
+// // Middleware to parse JSON data
+// app.use(express.json());
+// // Middleware to parse URL-encoded data
+// app.use(express.urlencoded({ extended: true }));
 
-app.use('/', require('./routes/employee'));
+// app.use('/', require('./routes/employee'));
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('the server is running');
